744bf53b08983912656db71a9064579b8a00cc97
Christopher <highlander.all@gmail.com>
2023-01-23 17:48:29 -0300

    style: Alterar indentação

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ import {pipeline} from 'stream/promises';
 import { createWriteStream } from 'fs';
 
 async function requestHandler(request, response) {
-  const fileName = `file-${randomUUID()}.csv`;
-  await pipeline(
-    request,
-    createWriteStream(fileName),
-  );
-  response.end('File uploaded successfully');
+	const fileName = `file-${randomUUID()}.csv`;
+
+	await pipeline(
+		request,
+		createWriteStream(fileName),
+	);
+  
+	response.end('File uploaded successfully');
 }
 
 export default createServer(requestHandler)
-  .listen(3000, () => console.log('Server is running on port 3000'));
+	.listen(3000, () => console.log('Server is running on port 3000'));
