2b23e4f9e7bbe5ae202df153744bada155b4cd34
Christopher <highlander.all@gmail.com>
2023-03-13 11:23:20 -0300

    feat: implementar 'job' para executar a rotina a cada 10 seg

---
+++ job.js
@@ -0,0 +1,21 @@
+import CronJob from 'node-cron';
+import { exec } from 'child_process';
+
+const job = CronJob.schedule('*/10 * * * * *',
+	async function() {
+		exec('cd ./uploads && node index.js',
+			(
+				err,
+				stdout,
+			) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				console.log(stdout);
+			});
+	});
+
+await job.start();
+
+export default job;
